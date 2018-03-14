eb3a91bdab92cd0ff1e85a083d642e431160565b
dhabhai-srlife <hdhabhai@srlife.net>
2018-03-14 11:49:17 -0400

    Initial Commit

---
+++ testStrings/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  testStrings
+//
+//  Created by ITMAC1 on 9/21/17.
+//  Copyright © 2017 Senior Life Insurance Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    
+    // declaring a variable
+    NSString *firstName;
+    // initializing a variable
+    firstName = @"Harsh";
+    
+    NSLog(@"First Name: %@", firstName);
+    
+    return 0;
+}
